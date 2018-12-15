f96dc72213f9b936846e191036d90cad00890429
chiubaca <alexchiu11@gmail.com>
2018-12-15 17:17:54 +0000

    seperated leaflet into a seperate component

--- src/components/journeyplanner/Geo-To-Postcode.vue
+++ src/components/journeyplanner/Geo-To-Postcode.vue
@@ -8,64 +8,34 @@
     clicking/onhover on the lines displays additional information about the steps
   clicking/onhover on the points displaus information about the streetName-->
   <div>
-    <div id="mapid"></div>
-    <div v-bind:class="{inputA:isStartSelected}" v-on:click="toggleStart">Start: {{startPointer}}</div>
-    <div v-bind:class="{inputB:isDestSelected}" v-on:click="toggleDest">Destination: {{destPointer}}</div>
-    <button>Search</button>
+    <leaflet-map></leaflet-map>
+    
+    <div v-bind:class="{inputA:isStartSelected}" v-on:click="toggleStart">Start: {{startLocation}}</div>
+    <div v-bind:class="{inputB:isDestSelected}" v-on:click="toggleDest">Destination: {{destLocation}}</div>
+    <button v-on:click="test">Test</button>
   </div>
 </template>
 
 <script>
-import L from "leaflet";
-import "leaflet/dist/leaflet.css";
-
-/* fix for marker icons not showing - 
-https://github.com/KoRiGaN/Vue2Leaflet/issues/157 */
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-  iconUrl: require("leaflet/dist/images/marker-icon.png"),
-  shadowUrl: require("leaflet/dist/images/marker-shadow.png")
-});
+import leafletMap from "./Leaflet-Map.vue";
 
 export default {
+  components:{
+    "leaflet-map": leafletMap   
+  },
   data: function() {
     return {
       count: 0,
-      startPointer: "",
-      destPointer: "",
-      isStartSelected: false,
-      isDestSelected: true
+      startLocation: "test",
+      destLocation: "",
+      isStartSelected: true,
+      isDestSelected: false
     };
   },
   computed: {
   },
   methods: {
-    initMap: function() {
-      var myIcon = L.icon({
-        iconUrl: require("../../assets/iconA.png"),
-        iconSize: [30, 30],
-      });
 
-      //Set starting point for the map
-      let map = L.map("mapid").setView([51.505, -0.09], 13);
-      //Set tile layer provider
-      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      }).addTo(map);
-
-      L.marker([51.5, -0.1], {icon: myIcon}).addTo(map);
-      
-      //init map click events
-      map.on("click", function(ev) {
-        // L.marker(ev.latlng, {icon: myIcon}).addTo(map); // ev is an event object (MouseEvent in this case)
-        
-        //check if isStartSelected and length is greater than 0
-            //make the click location = 
-      
-      });
-    },
     toggleStart: function() {
         //if startIsSelected is false 
         if(this.isStartSelected === false){
@@ -96,10 +66,13 @@ export default {
           //alert user to click on a location
           alert("Click on a destnation location")
         }
-    }
+    },
+    test: function(){
+        console.log("Testing 123")
+    } 
   },
   mounted() {
-    this.initMap();
+
   }
 };
 </script>
