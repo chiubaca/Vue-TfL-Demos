946a5f3778fcd41f20b6cce77d5bd537d6a114de
chiubaca <alexchiu11@gmail.com>
2018-12-26 18:38:58 +0000

    combined journey planner and tfl routes components

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,25 @@
   
   <div id="app">
   <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">    
+    <hr>
+    <h1>Dynamically Draws Tube Lines</h1>
+    <tflRoutes/>
+    <hr>
+    <h1>Simple Interactive Journey Planner </h1>
     <basicJourneyPlanner />
-    
-     
+    <hr> 
   </div>
 </template>
 
 <script>
 import basicJourneyPlanner from './components/BasicJourneyPlanner.vue'
+import tflRoutes from './components/TfLRoutes.vue'
 
 export default {
   name: 'app',
   components: {
-    basicJourneyPlanner
+    basicJourneyPlanner,
+    tflRoutes
   },
   data: function () {
     return {
